e056bb8a4d087a38bb4e0cb0062d9179116d2c5f
jimbo <j.kam.j.kan.w.jmj@gmail.com>
2016-10-27 14:59:52 +0900

    time-size

--- src/client.cpp
+++ src/client.cpp
@@ -14,6 +14,10 @@ double get_wall_time(){
 extern cybozu::RandomGenerator rg;
 
 int main(int argc,char* argv[]){
+	double start_sec = get_wall_time();
+	double s_time,e_time,calc_time=0,total_time=0;
+	s_time = start_sec;
+
   std::string str;
 	int sock;
 	int port = 4444;
@@ -45,43 +49,72 @@ int main(int argc,char* argv[]){
   PPDPP::Client client(str);	
   client.core = 1;
   
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
+
 	sock = prepCSock((char *)hostname.c_str(),port);
 
+	s_time = get_wall_time();
   client.makeParam(cparam);
-  
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
+
 	sendsize+=sendFile(sock,(char *)cparam.c_str());
 	recvsize+=recvFile(sock,(char *)sparam.c_str());
 
+	s_time = get_wall_time();
   client.setParam(sparam);
-  
   client.setKeys(prvfile, pubfile);
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
+
 	sendsize+=sendFile(sock,(char *)pubfile.c_str());
-  
+
+	s_time = get_wall_time(); 
   int sl = client.len_server;
   int cl = client.len_client;
   int epsilon = client.epsilon;
-
   std::vector< std::pair<int,int> > cells;
 	int li;
-    
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;  
+
   for(int i=0;i<sl+cl-1;i++){
+		s_time = get_wall_time(); 
     li = PPDPP::makePairVec(i,cl,sl,epsilon,cells);
     client.setLindex(li,cells);
     client.makeQuerySet(queryfile,cells);
+		e_time = get_wall_time();
+		calc_time += e_time-s_time;
 
 		sendsize+=sendFile(sock,(char *)queryfile.c_str());
 		recvsize+=recvFile(sock,(char *)resultfile.c_str());
 
+		s_time = get_wall_time();
     client.decResultSet(resultfile,cells);
+		e_time = get_wall_time();
+		calc_time += e_time-s_time;
     if(li != -1){
+			s_time = get_wall_time();
       client.makeLQuery(l_queryfile);
+			e_time = get_wall_time();
+			calc_time += e_time-s_time;
 
 			sendsize+=sendFile(sock,(char *)l_queryfile.c_str());
     }
   }
 	recvsize+=recvFile(sock,(char *)ansfile.c_str());
+
+	s_time = get_wall_time();
   int ans = client.decEditD(ansfile);
-  std::cout << "Edit distance : " << ans << std::endl;
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
+	total_time = get_wall_time() - start_sec;
+ 
+	std::cout << "Edit distance : " << ans << std::endl;
+	std::cout << "client calc_time : " << calc_time << "s" << std::endl;
+	std::cout << "total time : " << total_time << "s" << std::endl;
+	std::cout << "comsize : " << (double)(recvsize+sendsize)/(1024*1024) << "MB" << std::endl;
 
   return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -14,6 +14,10 @@ double get_wall_time(){
 extern cybozu::RandomGenerator rg;
 
 int main(int argc,char* argv[]){
+	double start_sec = get_wall_time();
+	double s_time,e_time,calc_time=0;
+	s_time = start_sec;
+
   std::string str;
 	int sock;
 	int port = 4444;	
@@ -42,17 +46,28 @@ int main(int argc,char* argv[]){
   PPDPP::Server server(str);
   server.core = 2;
   
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
+
 	sock = prepSSock(port);
 	sock = acceptSSock(sock);
 
+	s_time = get_wall_time();
   server.makeParam(sparam);
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
 
 	recvFile(sock,(char *)cparam.c_str());
 	sendFile(sock,(char *)sparam.c_str());
 
+	s_time = get_wall_time();
   server.setParam(cparam);
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;
   
 	recvFile(sock,(char *)pubfile.c_str());
+
+	s_time = get_wall_time();
   server.readPubkey(pubfile);
   
   int sl = server.len_server;
@@ -61,24 +76,41 @@ int main(int argc,char* argv[]){
 
   std::vector< std::pair<int,int> > cells;
 	int li;
-    
+	e_time = get_wall_time();
+	calc_time += e_time-s_time;  
+  
   for(int i=0;i<sl+cl-1;i++){
+		s_time = get_wall_time();
     li = PPDPP::makePairVec(i,cl,sl,epsilon,cells);
     server.setLindex(li,cells);
+		e_time = get_wall_time();
+		calc_time += e_time-s_time; 
 
 		recvFile(sock,(char *)queryfile.c_str());
 
+		s_time = get_wall_time();
     server.parallelDP(queryfile,resultfile,cells);
+		e_time = get_wall_time();
+		calc_time += e_time-s_time; 
 
 		sendFile(sock,(char *)resultfile.c_str());
     if(li != -1){
 			recvFile(sock,(char *)l_queryfile.c_str());
     	
+			s_time = get_wall_time();
   		server.addAnsVec(l_queryfile);
+			e_time = get_wall_time();
+			calc_time += e_time-s_time; 
     }
   }
+	s_time = get_wall_time();
   server.makeEditDFile(ansfile);
+	e_time = get_wall_time();
+	calc_time += e_time-s_time; 
+
 	sendFile(sock,(char *)ansfile.c_str());
+
+	std::cout << "server calc_time : " << calc_time << "s" << std::endl;
 
   return 0;
 }
