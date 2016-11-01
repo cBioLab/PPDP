0dea7c2b312269ebc387d0850696af8610e28e5c
jimbo <j.kam.j.kan.w.jmj@gmail.com>
2016-11-01 12:45:10 +0900

    +data26

--- data/makesequence.py
+++ data/makesequence.py
@@ -3,17 +3,19 @@ import random
 source = 'qwertyuioplkjhgfdsazxcvbnm'
 filem = '.dat'
 
-for l in xrange(1,101):
+for l in xrange(1,11):
 	str1 = ""
-	for i in xrange(random.randint(5,20)):
+	for i in xrange(0,l*10):
 		str1 = str1 + random.choice(source)
 	str2 = ""
-	for i in xrange(random.randint(5,20)):
+	for i in xrange(0,l*10):
 		str2 = str2 + random.choice(source)
-	filename = 'test3/' + str(l) + filem
-	f = open(filename,'w')
-	f.write(str1)
-	f.write('\n')
-	f.write(str2)
-	f.close()
+	filename1 = 'server_26/' + str(l*10) + filem
+	filename2 = 'client_26/' + str(l*10) + filem
+	f1 = open(filename1,'w')
+	f1.write(str1)
+	f1.close
+	f2 = open(filename2,'w')
+	f2.write(str2)
+	f2.close()
 
