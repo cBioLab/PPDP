9305420f268787f989cf56d098ab3d84b715fb39
jimbo <j.kam.j.kan.w.jmj@gmail.com>
2016-12-10 13:25:30 +0900

    d_data

--- data/makebits.py
+++ data/makebits.py
@@ -3,19 +3,24 @@ import random
 source = '01'
 filem = '.dat'
 
-
-str1 = ""
-for i in xrange(0,10000):
-        str1 = str1 + random.choice(source)
-str2 = ""
-for i in xrange(0,2000):
-        str2 = str2 + random.choice(source)
-filename1 = 'server_d/bits_s' + filem
-filename2 = 'client_d/bits_c' + filem
-f1 = open(filename1,'w')
-f1.write(str1)
-f1.close
-f2 = open(filename2,'w')
-f2.write(str2)
-f2.close()
+for l in xrange(1,11):
+	DNA1 = ""
+#	for i in xrange(random.randint(5,20)):
+	for i in xrange(l*10):
+		DNA1 = DNA1 + random.choice(source)
+	DNA2 = ""
+#	for i in xrange(random.randint(5,20)):
+	for i in xrange(l*10):
+		DNA2 = DNA2 + random.choice(source)
+#	print DNA1
+#	print DNA2
+	filename1 = 'server_d/' + str(l*10) + filem
+	filename2 = 'client_d/' + str(l*10) + filem
+	f1 = open(filename1,'w')
+	f1.write(DNA1)
+	f1.close()
+#	f.write('\n')
+	f2 = open(filename2,'w')
+	f2.write(DNA2)
+	f2.close()
 
